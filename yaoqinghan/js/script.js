83db20fc8689fd8f429366378ea300116808a1eb
didiweis <1849094983@qq.com>
2017-12-15 17:25:51 +0800

    提交

--- yaoqinghan/js/script.js
+++ yaoqinghan/js/script.js
@@ -1,3 +1,15 @@
 /**
  * Created by lenovo on 17/12/15 015.
  */
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    // 如果需要前进后退按钮
+    navigation: {
+        nextEl: '.swiper-button-next',
+
+    },
+});
